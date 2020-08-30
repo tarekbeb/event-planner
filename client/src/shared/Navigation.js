869dc3808872a116638a5ebee6620d44546c01c3
tarekbeb <tariqbeb@gmail.com>
2020-08-30 01:09:17 -0500

    initiated routes and react app

---
+++ client/src/shared/Navigation.js
@@ -0,0 +1,16 @@
+{/* <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel> */}
