80a9ec5dc7f203c3d9bdad44e44d70456141a3f5
tarekbeb <tariqbeb@gmail.com>
2020-09-07 11:17:59 -0500

    start navigation setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import { AuthPage, Events, Bookings } from './pages/index';
+import { Navigation } from './shared/index';
 
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Redirect from="/" to="/auth" exact />
-      <Route path="/auth" component={AuthPage} />
-      <Route path="/events" component={Events} />
-      <Route path="/bookings" component={Bookings} />
-    </Switch>
+    <Navigation />
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={Events} />
+        <Route path="/bookings" component={Bookings} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- client/src/shared/Navigation.js
+++ client/src/shared/Navigation.js
@@ -1,16 +1,39 @@
-{/* <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import { NavLink } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const Navigation = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          variant="fullWidth"
+          onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+          <NavLink to="/bookings">Bookings</NavLink>
+          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/authenticate">Authenticate</NavLink>
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel> */}
+    </div>
+  );
+}
+
+export default Navigation; 
